6d9591cff587a122e85f6b5b2dba7104d5604dec
5ans <miles.imber@googlemail.com>
2019-10-01 15:13:55 +0100

    First Text RPG Commit

---
+++ Project/Project/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using system;
+
+public class MyProgram
+{
+    public static void main(string[] args)
+    {
+        string yourName;
+
+        output("Ok retard, what is your name?");
+        yourName = inputText();
+    }
+
+    private static void output(string v)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static double inputValue()
+    {
+        double result
+        while (!double.TryParse(System.Console.ReadLine(), out result)) ;
+        return result
+    }
+
+    private static string inputText()
+    {
+        return Console.ReadLine()
+    }
+
+    private static output(string text)
+    {
+        Console.WriteLine(text)
+    }
+}
